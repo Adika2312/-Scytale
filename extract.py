098608eba1a92063077fca7f9a6624d2f9047757
Adi Karif <adika3@mta.ac.il>
2025-06-10 11:47:47 +0300

    finish extract

--- extract.py
+++ extract.py
@@ -1,9 +1,9 @@
 import os
 import requests
-import json
+import pandas as pd
 from dotenv import load_dotenv
 
-
+#set the request
 load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 REPO = "Scytale-exercise/scytale-repo3"
@@ -14,6 +14,41 @@ headers = {
     "Accept": "application/vnd.github+json"
 }
 
+params = {
+    "state": "closed",
+    "per_page": 100
+}
+
+#send the request
+response = requests.get(BASE_URL, headers=headers, params=params)
+
+#check if the request was successful
+if response.status_code == 200:
+    pull_requests = response.json()
+    merged_pull_requests = [pr for pr in pull_requests if pr.get("merged_at") is not None]
+
+    print(f"Found {len(merged_pull_requests)} merged PRs")
+
+    #create csv
+    pr_data = [
+        {
+        "number":pr.get("number"),
+        "title":pr.get("title"),
+        "author":pr.get("user").get("login"),
+        "merged_at":pr.get("merged_at"),
+        "CR_password": None,
+        "CHECKS_PASSED":None
+        }
+        for pr in merged_pull_requests
+    ]
+
+    df = pd.DataFrame(pr_data)
+    df.to_csv("data/raw_prs.csv", index=False)
+
+    print("Saved to data/raw_prs.csv")
+
+else:
+    print("Failed to fetch PRs:", response.status_code, response.text)
 
 
 
